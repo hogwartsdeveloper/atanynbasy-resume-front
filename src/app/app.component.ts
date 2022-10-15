69a20c4a981d6bd11bdb6cd8182c12976b4d4548
zhannur <jannuraidynuly@gmail.com>
2022-10-15 16:25:59 +0600

    init project and connect socket

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MessageService} from "./message.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'atanynbasy-front';
+
+  constructor(private messageService: MessageService) {}
+
+  ngOnInit() {
+    this.messageService.getNewMessage().subscribe(msg => console.log(msg))
+  }
+
+  sendMessage() {
+    this.messageService.sendMessage('Altosh')
+  }
 }
