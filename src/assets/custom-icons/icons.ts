c447e50ca3c5ca6cb87c2f96557e8c9e9f6e2a03
zhannur <jannuraidynuly@gmail.com>
2022-10-29 12:15:23 +0600

    add fantastic icons

--- src/assets/custom-icons/icons.ts
+++ src/assets/custom-icons/icons.ts
@@ -1,13 +1,17 @@
 export type IconsId =
+  | 'linkedin'
   | 'logo';
 
 export type IconsKey =
+  | 'Linkedin'
   | 'Logo';
 
 export enum Icons {
+  Linkedin = 'linkedin',
   Logo = 'logo',
 }
 
 export const ICONS_CODEPOINTS: { [key in Icons]: string } = {
-  [Icons.Logo]: '61697',
+  [Icons.Linkedin]: '61697',
+  [Icons.Logo]: '61698',
 };
