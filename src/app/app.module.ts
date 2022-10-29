5fbd2154cb11d46c33ec07c0872722970a9c9ab3
zhannur <jannuraidynuly@gmail.com>
2022-10-29 11:31:56 +0600

    toolbar logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {SocketIoConfig, SocketIoModule} from "ngx-socket-io";
 import { PlanetComponent } from './planet/planet.component';
+import {ToolbarModule} from "./modules/toolbar/toolbar.module";
 
 const socketConfig: SocketIoConfig = { url: 'http://localhost:3000', options: {}}
 
@@ -12,10 +13,11 @@ const socketConfig: SocketIoConfig = { url: 'http://localhost:3000', options: {}
     AppComponent,
     PlanetComponent
   ],
-  imports: [
-    BrowserModule,
-    SocketIoModule.forRoot(socketConfig),
-  ],
+    imports: [
+        BrowserModule,
+        SocketIoModule.forRoot(socketConfig),
+        ToolbarModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
